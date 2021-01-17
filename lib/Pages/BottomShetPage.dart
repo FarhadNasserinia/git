28af16673dbd448c3d2fe9aa4e3982a3f7e67d70
FarhadNasserinia <nasserinia.farhad@gmail.com>
2021-01-17 20:32:48 +0330

    clean code in page : BottomShetPage.dart

--- lib/Pages/BottomShetPage.dart
+++ lib/Pages/BottomShetPage.dart
@@ -7,50 +7,17 @@ class BottomSheetPage extends StatefulWidget {
 }
 
 class _BottomSheetPageState extends State<BottomSheetPage> {
-
-  // test for commit for get documents
-  // to new branch
-
-
-  // now should be new farhad branch working
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-          height: size.height,
-          width: size.width * 0.2,
-          color: Colors.yellow,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                flex: 20,
-                child: ListView(
-                  children: <Widget>[
-                    TextFormField(),
-                    TextFormField(),
-                    TextFormField(),
-                    TextFormField(),
-                    TextFormField(),
-
-                  ],
-                ),
-              ),
-              Flexible(
-                  flex: 1,
-                  fit: FlexFit.loose,
-                  child: Container(width: size.width, child: RaisedButton(color: Colors.red, child: Text('BottomSheet 2'), onPressed: () {})))
-            ],
-          )),
-      // bottomSheet: Container(
-      //     margin: const EdgeInsets.all(0.0),
-      //     width: size.width,
-      //     child: RaisedButton(color: Colors.red, child: Text('BottomSheet'), onPressed: () {})),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // floatingActionButton:
-      //     Container(width: size.width, margin: const EdgeInsets.all(0.0), child: RaisedButton(color: Colors.red, child: Text('FloatingActionButton'), onPressed: () {})),
+        height: size.height,
+        width: size.width,
+        color: Colors.yellow,
+        child: Text('${size.height * 0.2}'),
+      ),
     );
   }
 }
